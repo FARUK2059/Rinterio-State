bdb3f1a5eeb9080122cf28f6ccaea7ccf4dd11d7
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-23 05:07:01 +0600

    Next.js Stup Complet

---
+++ src/components/HomePge/HomePage.tsx
@@ -0,0 +1,18 @@
+import Navbar from "../Shared/Navbar";
+
+
+
+const HomePage = () => {
+    return (
+        <div>
+            
+            {/* Nevbar section */}
+            <div>
+                <Navbar></Navbar>
+            </div>
+
+        </div>
+    );
+};
+
+export default HomePage;
