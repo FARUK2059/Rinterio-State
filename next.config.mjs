1cb941a9b27ec158009e37c70c26e0797db6f3ac
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-23 16:27:49 +0600

    Setup Navber

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "*",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  export default nextConfig;
+  
